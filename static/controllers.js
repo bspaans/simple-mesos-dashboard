17dfb3983f73ae105f700f370e0e5909b8ee2532
Bart Spaans <bart.spaans@opencredo.com>
2015-04-08 11:05:24 +0100

    Showing stats using angular

---
+++ static/controllers.js
@@ -0,0 +1,15 @@
+var dashboard = angular.module('mesos-dashboard', [])
+
+
+dashboard.config(['$httpProvider', function($httpProvider) {
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+}])
+
+
+dashboard.controller('dashboardController', ['$scope', '$http', function($scope, $http) {
+
+  $http.get('/api/nodes/stats').success(function(data) {
+    $scope.stats = data;
+  });
+}]);
