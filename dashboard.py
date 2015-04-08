b57111568fce59c55a898705fef43de618391330
Bart Spaans <bart.spaans@opencredo.com>
2015-04-08 11:39:42 +0100

    Listen on 0.0.0.0

--- dashboard.py
+++ dashboard.py
@@ -69,4 +69,4 @@ app = Flask(__name__)
 api = Api(app)
 api.add_resource(NodeStatisticsResource, '/api/nodes/stats')
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
