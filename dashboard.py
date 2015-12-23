fcd886f28c570496ef8642405e11fef43e2fda6b
Bart Spaans <bart.spaans@opencredo.com>
2015-12-23 18:27:02 +0100

    Added a node and frameworks view. Work in progress.

--- dashboard.py
+++ dashboard.py
@@ -3,80 +3,197 @@
 import os 
 import requests
 import json
+import sys
 from flask import Flask
 from flask.ext.restful import Api, Resource
 
 MESOS=os.environ.get('MESOS', None)
-if MESOS is None:
-    raise Exception("Missing MESOS environment variable")
+TEST_MODE = False
 
 class Node(object):
-    def __init__(self, node_id):
+    def __init__(self, node_id, hostname):
         self.node_id = node_id
+        self.hostname = hostname
         self.resources = []
         self.cpu_total = 0.0
         self.mem_total = 0.0
+        self.max_mem   = 0.0
+        self.max_cpu   = 0.0
+        self.tasks     = []
 
-    def add_resources(self, framework_name, resources):
-        mem = resources.get('mem', 0.0)
-        cpu = resources.get('cpus', 0.0)
+    def add_resources(self, framework_name, cpu, mem):
         self.cpu_total += cpu 
         self.mem_total += mem 
         self.resources.append((framework_name, cpu, mem))
 
-    def set_hostname(self, hostname):
-        self.hostname = hostname
+    def set_max_resources(self, max_mem, max_cpu):
+        self.max_mem = max_mem
+        self.max_cpu = max_cpu
 
-    def set_max_resources(self, resources):
-        self.max_mem = resources.get('mem', 0.0)
-        self.max_cpu = resources.get('cpus', 0.0)
+    def add_task(self, task):
+        self.tasks.append(task)
+        if task.is_running():
+            self.add_resources(task.framework.name, task.cpu, task.mem)
+
+    def _tasks_to_dict(self):
+        result = {}
+        for t in self.tasks:
+            task_dict = t.to_dict()
+            result[task_dict["id"]] = task_dict
+        return result
 
     def to_dict(self):
         return {'id': self.node_id, 'hostname': self.hostname,
             'mem': self.mem_total, 'cpu': self.cpu_total,
-            'max_cpu': self.max_cpu, 'max_mem': self.max_mem}
+            'max_cpu': self.max_cpu, 'max_mem': self.max_mem,
+            'tasks': self._tasks_to_dict()}
+
+class Nodes(object):
+    def __init__(self, nodes):
+        self.nodes = nodes
+
+    def get_by_id(self, node_id):
+        return self.nodes[node_id]
+
+    def to_dict(self):
+        result = {}
+        for n in self.nodes.itervalues():
+            node_dict = n.to_dict()
+            result[node_dict["id"]] = node_dict
+        return result
+
+class Framework(object):
+    def __init__(self, framework_id, name, hostname, active):
+        self.framework_id = framework_id
+        self.name = name
+        self.hostname = hostname
+        self.active = active
+        self.tasks = {}
+        self.completed_tasks = {}
+
+    def set_completed_tasks(self, tasks):
+        self.completed_tasks = tasks
+
+    def set_tasks(self, tasks):
+        self.tasks = tasks
+
+    def to_dict(self):
+        return {"id": self.framework_id,
+                "name": self.name,
+                "hostname": self.hostname,
+                "active": self.active}
+
+class Task(object):
+    def __init__(self, framework, task_id, name, state):
+        self.framework = framework
+        self.task_id = task_id
+        self.name = name
+        self.cpu = 0.0
+        self.mem = 0.0
+        self.disk = 0.0
+        self.node = None
+        self.state = state
+
+    def is_running(self):
+        return self.state == "TASK_RUNNING"
+
+    def set_resources(self, cpu, mem, disk):
+        self.cpu = cpu
+        self.mem = mem
+        self.disk = disk
+
+    def set_node(self, node):
+        self.node = node
+
+    def to_dict(self):
+        return {"framework_id": self.framework.framework_id,
+                "name": self.name,
+                "id": self.task_id,
+                "cpu": self.cpu,
+                "mem": self.mem,
+                "disk": self.disk,
+                "node_id": self.node.node_id,
+                "state": self.state}
+
+class MesosResponseParser(object):
+    def __init__(self):
+        self.nodes = None
+        self.frameworks = {}
+
+    def parse(self, json_payload):
+        self.nodes = self._parse_nodes(json_payload['slaves'])
+        self.frameworks = self._parse_frameworks(json_payload['frameworks'])
+
+    def _parse_nodes(self, nodes):
+        result = {}
+        for slave in nodes:
+            node_id = slave['id']
+            node = result.get(node_id, Node(node_id, slave['hostname']))
+            node.set_max_resources(slave['resources']['mem'], slave['resources']['cpus'])
+            result[node_id] = node
+        return Nodes(result)
+
+    def _parse_frameworks(self, frameworks):
+        result = {}
+        for framework in frameworks:
+            framework_id = framework["id"]
+            name = framework["name"]
+            hostname = framework["hostname"]
+            active = framework["active"]
+            f = Framework(framework_id, name, hostname, active)
+            f.set_tasks(self._parse_tasks(f, framework["tasks"]))
+            f.set_completed_tasks(self._parse_tasks(f, framework["completed_tasks"]))
+            result[framework_id] = f
+        return result
+
+    def _parse_tasks(self, framework, tasks):
+        result = {}
+        for task in tasks:
+            task_id = task["id"]
+            name = task["name"]
+            status = task["state"]
+            t = Task(framework, task_id, name, status)
+
+            cpu = task["resources"]["cpus"]
+            mem = task["resources"]["mem"]
+            disk = task["resources"]["disk"]
+            t.set_resources(cpu, mem, disk)
+
+            slave_id = task["slave_id"]
+            node = self.nodes.get_by_id(slave_id)
+            node.add_task(t)
+            t.set_node(node)
+            result[task_id] = t
+        return result
 
 class NodeStatisticsResource(Resource):
-    def mesos_endpoint(self, mesos_host = None):
+    def _mesos_endpoint(self, mesos_host = None):
         if mesos_host is None:
             mesos_host = MESOS
         return mesos_host + "/state.json"
 
-    def process_frameworks(self, nodes, frameworks):
-        for framework in frameworks:
-            name = framework['name']
-            for task in framework['tasks']:
-                node_id = task['slave_id']
-                node = nodes.get(node_id, Node(node_id))
-                node.add_resources(name, task['resources'])
-                nodes[node_id] = node
-        return map(lambda n: n.to_dict(), nodes.values())
-
-    def process_slaves(self, slaves):
-        nodes = {}
-        for slave in slaves:
-            node_id = slave['id']
-            node = nodes.get(node_id, Node(node_id))
-            node.set_max_resources(slave['resources'])
-            node.set_hostname(slave['hostname'])
-            nodes[node_id] = node
-        return nodes
-
-    def get_master_from_payload(self, payload):
+    def _get_master_from_payload(self, payload):
         master = payload["leader"]
         return "http://" + master[len("master@"):]
 
-
     def get(self):
-        payload = requests.get(self.mesos_endpoint()).text
+        if not TEST_MODE:
+            payload = requests.get(self._mesos_endpoint()).text
+        else: 
+            with open("tests/test_response.json") as f:
+                payload = f.read()
         json_payload = json.loads(payload)
-        master = self.get_master_from_payload(json_payload)
+        master = self._get_master_from_payload(json_payload)
         if master != MESOS:
-            payload = requests.get(self.mesos_endpoint(master)).text
+            payload = requests.get(self._mesos_endpoint(master)).text
             json_payload = json.loads(payload)
-        nodes = self.process_slaves(json_payload['slaves'])
-        node_breakdown = self.process_frameworks(nodes, json_payload['frameworks'])
-        return {"nodes": node_breakdown, "master": master}
+        parser = MesosResponseParser()
+        parser.parse(json_payload)
+        node_breakdown = parser.nodes.to_dict()
+        frameworks = {}
+        for id_, f in parser.frameworks.iteritems():
+            frameworks[id_] = f.to_dict()
+        return {"nodes": node_breakdown, "master": master, "frameworks": frameworks}
 
 app = Flask(__name__)
 api = Api(app)
@@ -90,6 +207,10 @@ def index():
     f.close()
     return index, 200
 
-
 if __name__ == '__main__':
+    if MESOS is None:
+        if "--test-mode" in sys.argv:
+            TEST_MODE = True
+        else:
+            raise Exception("Missing MESOS environment variable and not in --test-mode")
     app.run(host='0.0.0.0', debug=True)
--- static/controllers.js
+++ static/controllers.js
@@ -1,4 +1,54 @@
-var dashboard = angular.module('mesos-dashboard', [])
+var dashboardApp = angular.module('dashboardApp', [
+    'ngRoute',
+    'dashboardControllers'
+]);
+
+dashboardApp.factory('statsService', ['$interval', '$http', function($interval, $http) {
+    var service = {};
+    service.master = null;
+    service.stats = {};
+    service.frameworks = [];
+    updateStats = function() {
+        $http.get('/api/nodes/stats').success(function(data) {
+            service.master = data.master;
+            service.stats = data.nodes;
+            service.frameworks = data.frameworks;
+        });
+    }
+    updateStats();
+    $interval(updateStats, 3000);
+    return service;
+}]);
+dashboardApp.factory('pageService', [function() {
+    var current = {'page': 'overview'}
+    return current;
+}]);
+
+dashboardApp.config(['$routeProvider',
+        function($routeProvider) {
+            $routeProvider.
+                when('/overview', {
+                    templateUrl: 'static/partials/overview.html',
+                    controller: 'overviewController'
+                }).
+                when('/frameworks', {
+                    templateUrl: 'static/partials/frameworks.html',
+                    controller: 'frameworksController'
+                }).
+                when('/nodes', {
+                    templateUrl: 'static/partials/nodes.html',
+                    controller: 'nodeController'
+                }).
+                when('/nodes/:nodeId', {
+                    templateUrl: 'static/partials/node.html',
+                    controller: 'nodeController'
+                }).
+                otherwise({
+                    redirectTo: '/overview'
+                });
+        }]);
+
+var dashboard = angular.module('dashboardControllers', [])
 
 
 dashboard.config(['$httpProvider', function($httpProvider) {
@@ -7,10 +57,20 @@ dashboard.config(['$httpProvider', function($httpProvider) {
 }])
 
 
-dashboard.controller('dashboardController', ['$scope', '$http', function($scope, $http) {
-
-  $http.get('/api/nodes/stats').success(function(data) {
-    $scope.master = data.master;
-    $scope.stats = data.nodes;
-  });
+dashboard.controller('overviewController', ['$scope', 'statsService', 'pageService', function($scope, statsService, pageService) {
+    $scope.statsService = statsService;
+    pageService.page = 'overview';
+}]);
+dashboard.controller('frameworksController', ['$scope', 'statsService', 'pageService', function($scope, statsService, pageService) {
+    $scope.statsService = statsService;
+    pageService.page = 'frameworks';
+}]);
+dashboard.controller('nodeController', ['$scope', 'statsService', '$routeParams', 'pageService',
+function($scope, statsService, $routeParams, pageService) {
+    $scope.nodeId = $routeParams.nodeId;
+    $scope.statsService = statsService;
+    pageService.page = 'nodes';
+}]);
+dashboard.controller('navController', ['$scope', 'pageService', function($scope, pageService) {
+    $scope.pageService = pageService;
 }]);
